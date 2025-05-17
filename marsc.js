1e5e8a4b1806de0d4c7295db7e5367604f423d27
LinMandarin <rostislavglusak@gmail.com>
2025-05-17 11:20:51 +0300

    add home

--- marsc.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    document.querySelectorAll(".favorite").forEach(button => {
-        button.addEventListener("click", () => {
-            button.classList.toggle("active");
-        });
-    });
-});
